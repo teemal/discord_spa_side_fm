302092983cdd5de3d7832aeabbe4c7a38d4a103c
teemal <taymal1987@gmail.com>
2020-06-10 15:36:47 -0600

    start basic help command

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`); 
 });
 client.on('message', msg => {  
-    if(message.author.bot) return;
-    if (msg.content === 'ping') {     
-        msg.reply('Pong!');   
-    } 
+    // if(message.author.bot) return;
+    message = msg.content.split('!')
+    console.log(message)
+    if(message[0] === ''){
+        if(message[1] === 'ping'){     
+            msg.reply('Pong!');   
+        }
+        if(message[1] === 'help'){     
+            msg.reply('sliding into your DM\'s :sunglasses: \n');
+            msg.author.send('test')
+        }
+    }
 });
 client.login(process.env.TOKEN);
